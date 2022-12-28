5c3f28ef9188310717f373cd886e34692c2dd379
math-prog <matheus131998@gmail.com>
2022-12-28 19:25:15 -0300

    added folders

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='EDA on evolution hospital beds during COVID-19 in the State of Goias Brazil.',
+    author='Matheus da Silva',
+    license='MIT',
+)
