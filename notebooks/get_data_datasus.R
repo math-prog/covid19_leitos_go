f9d9c44a640317f1cd32dd9e015d0cd731e200ac
math-prog <matheus131998@gmail.com>
2022-12-31 13:00:57 -0300

    created RScript to get data CNES-LT

--- notebooks/get_data_datasus.R
+++ notebooks/get_data_datasus.R
@@ -1,8 +1,13 @@
 #Obter dados datasus atraves da biblioteca microdatasus
+# Baixar biblioteca
+#remotes::install_github("rfsaldanha/microdatasus", force = TRUE)
 
-install.packages("remotes")
-remotes::install_github("rfsaldanha/microdatasus", force = TRUE)
-
+#Carrega biblioteca
 library(microdatasus)
+
+#Baixar dados referente aos leitos no estado de GO, no periodo de jan/19 a nov/22
 dados <- fetch_datasus(year_start = 2019, year_end = 2022, month_start = 1, month_end = 11, uf = "GO", information_system = "CNES-LT")
-dados <- process_sim(dados)
+#dados <- process_cnes(dados, information_system = "CNES-ST")
+
+# Grava os dados em .csv
+write.csv(dados, file = "../data/raw/cnes_lt-2019_2022.csv", row.names = TRUE)
